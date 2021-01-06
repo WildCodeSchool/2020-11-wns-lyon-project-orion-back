7a28701447573a032db00c78b16ab5ac52382fcf
Riri <riri.zamor@gmail.com>
2021-01-06 14:52:31 +0100

    remove .env

--- src/api/post/post.entity.ts
+++ src/api/post/post.entity.ts
@@ -9,14 +9,14 @@ import {
     UpdateDateColumn,
     DeleteDateColumn,
 } from 'typeorm';
-import { Field, Int, ObjectType } from '@nestjs/graphql';
-import { User } from '@api/user/user.entity';
-import { Star } from '@api/star/star.entity';
-import { Like } from '@api/like/like.entity';
-import { PostTypes } from './enums/post-type.enum';
+import {Field, Int, ObjectType} from '@nestjs/graphql';
+import {User} from '@api/user/user.entity';
+import {Star} from '@api/star/star.entity';
+import {Like} from '@api/like/like.entity';
+import {PostTypes} from './enums/post-type.enum';
 
 @ObjectType()
-@Entity({ name: 'posts' })
+@Entity({name: 'posts'})
 export class Post {
     @Field(() => Int)
     @PrimaryGeneratedColumn()
@@ -27,7 +27,7 @@ export class Post {
     readonly content: string;
 
     @Field(() => PostTypes)
-    @Column('enum', { enum: PostTypes, default: PostTypes.default })
+    @Column('enum', {enum: PostTypes, default: PostTypes.default})
     readonly type: PostTypes;
 
     @Field()
@@ -53,35 +53,35 @@ RELATIONS
     )
     readonly author: Promise<User>;
 
-    @Field(() => Post, { nullable: true })
+    @Field(() => Post, {nullable: true})
     @ManyToOne(
         () => Post,
         post => post.parent,
-        { nullable: true },
+        {nullable: true},
     ) //softDelete
     readonly parent?: Promise<Post>;
 
-    @Field(() => [Post], { nullable: true })
+    @Field(() => [Post], {nullable: true})
     @OneToMany(
         () => Post,
         post => post.children,
-        { nullable: true },
+        {nullable: true},
     )
     readonly children?: Promise<Post[]>;
 
-    @Field(() => Star, { nullable: true })
+    @Field(() => Star, {nullable: true})
     @OneToMany(
         () => Star,
         star => star.post,
-        { nullable: true, onDelete: 'CASCADE' },
+        {nullable: true, onDelete: 'CASCADE'},
     )
     readonly star: Promise<Star>;
 
-    @Field(() => [Like], { nullable: true })
+    @Field(() => [Like], {nullable: true})
     @OneToMany(
         () => Like,
         like => like.post,
-        { nullable: true, onDelete: 'CASCADE' },
+        {nullable: true, onDelete: 'CASCADE'},
     )
     readonly likes: Promise<Like[]>;
 
